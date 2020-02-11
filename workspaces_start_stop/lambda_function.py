f90d4fe55ab2503fcb0d589111f19d56cca13483
Leonardo Biffi <leonardobiffi@outlook.com>
2020-02-11 13:57:35 -0300

    added workspaces_start_stop script

---
+++ workspaces_start_stop/lambda_function.py
@@ -0,0 +1,28 @@
+import boto3
+
+# WorkspaceIds not to be in the script rule
+WORKSPACES_EXCEPTION = ['ws-c15w7xvfn']
+
+def execute_workspaces(command):
+	"""
+	This function execute start and stop workspaces where not in WORKSPACES_EXCEPTION
+	"""
+	client = boto3.client('workspaces')
+	response = client.describe_workspaces()['Workspaces']
+
+	# get WorkspacesIds
+	workspaceIds = [ {'WorkspaceId': workspace['WorkspaceId']} for workspace in response if workspace['WorkspaceId'] not in WORKSPACES_EXCEPTION ]
+
+	if command == 'start':
+		response = client.start_workspaces(StartWorkspaceRequests=workspaceIds)
+	
+	elif command == 'stop':
+		response = client.stop_workspaces(StopWorkspaceRequests=workspaceIds)	
+
+
+def lambda_handler(event, context):
+	"""
+	execute function execute_workspaces
+	@command (start, stop)
+	"""
+	execute_workspaces(command='stop')
