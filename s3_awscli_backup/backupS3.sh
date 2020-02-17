0765b8e29dc867a03b4248b7073e3f26d1dbfad5
Leonardo Biffi <leonardobiffi@outlook.com>
2020-02-17 14:54:13 -0300

    add aws_cli variable

--- s3_awscli_backup/backupS3.sh
+++ s3_awscli_backup/backupS3.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
+# 
+# Example in cron
+#	00 22 * * * /directory/backupS3.sh > /var/log/backupS3.log 2>&1
+#
 
 BUCKET_NAME="leonardobiffi"
 DIRECTORY_REMOTE=""
 DIRECTORY_LOCAL="/home/leonardo/mytec"
-
-timestamp=$(date +"%F %T")
-logfile="/home/leonardo/mytec/aws-utils/s3_awscli_backup/s3.log"
+AWS_CLI="/usr/local/bin/aws"
+TIMESTAMP=$(date +"%F %T")
+LOGFILE="/home/leonardo/mytec/aws-utils/s3_awscli_backup/s3.log"
 UPLOAD_LOG="/home/leonardo/mytec/aws-utils/s3_awscli_backup/upload.log"
-
 S3_DIRECTORY="s3://$BUCKET_NAME/$DIRECTORY_REMOTE"
 
 # command execute
-aws s3 sync $DIRECTORY_LOCAL $S3_DIRECTORY > $UPLOAD_LOG
+$AWS_CLI s3 sync $DIRECTORY_LOCAL $S3_DIRECTORY > $UPLOAD_LOG
 
 if [ $? -eq 0 ];then
-   echo "Backup Completed Successfully at $timestamp"  >> "$logfile"
+   echo "Backup Completed Successfully at $TIMESTAMP"  >> "$LOGFILE"
 else
-   echo "Backup Failed at $timestamp"  >> "$logfile"
+   echo "Backup Failed at $TIMESTAMP"  >> "$LOGFILE"
 fi
