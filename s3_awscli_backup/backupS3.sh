36b0a11216017f375c66097ef47a712cea6acfed
Leonardo Biffi <leonardobiffi@outlook.com>
2020-02-17 14:58:28 -0300

    alter variables name

--- s3_awscli_backup/backupS3.sh
+++ s3_awscli_backup/backupS3.sh
@@ -7,17 +7,18 @@
 BUCKET_NAME="leonardobiffi"
 DIRECTORY_REMOTE=""
 DIRECTORY_LOCAL="/home/leonardo/mytec"
+LOG_FILE="/home/leonardo/mytec/aws-utils/s3_awscli_backup/s3.log"
+LOG_UPLOAD="/home/leonardo/mytec/aws-utils/s3_awscli_backup/upload.log"
 AWS_CLI="/usr/local/bin/aws"
 TIMESTAMP=$(date +"%F %T")
-LOGFILE="/home/leonardo/mytec/aws-utils/s3_awscli_backup/s3.log"
-UPLOAD_LOG="/home/leonardo/mytec/aws-utils/s3_awscli_backup/upload.log"
+
 S3_DIRECTORY="s3://$BUCKET_NAME/$DIRECTORY_REMOTE"
 
 # command execute
-$AWS_CLI s3 sync $DIRECTORY_LOCAL $S3_DIRECTORY > $UPLOAD_LOG
+$AWS_CLI s3 sync $DIRECTORY_LOCAL $S3_DIRECTORY > $LOG_UPLOAD
 
 if [ $? -eq 0 ];then
-   echo "Backup Completed Successfully at $TIMESTAMP"  >> "$LOGFILE"
+   echo "Backup Completed Successfully at $TIMESTAMP"  >> "$LOG_FILE"
 else
-   echo "Backup Failed at $TIMESTAMP"  >> "$LOGFILE"
+   echo "Backup Failed at $TIMESTAMP"  >> "$LOG_FILE"
 fi
