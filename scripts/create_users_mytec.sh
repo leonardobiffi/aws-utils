67d3eb81d0d7ac5dddc4eeae01f86ca31f1edc3b
Leonardo Biffi <leonardobiffi@outlook.com>
2020-03-06 14:59:11 -0300

    added new user in script

--- scripts/create_users_mytec.sh
+++ scripts/create_users_mytec.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-users="matheus.mytec filipe.mytec lucas.mytec railander.mytec thailan.mytec andre.mytec"
+users="matheus.mytec filipe.mytec lucas.mytec railander.mytec thailan.mytec andre.mytec lucast.mytec"
 
 for user in $users
 do
-	aws iam create-user --user-name "$user"
-	aws iam create-login-profile --user-name "$user" --password Mytec@2020 --password-reset-required
-	aws iam add-user-to-group --user-name "$user" --group-name admin
+	aws iam create-user --user-name "$user" --profile $AWS_PROFILE
+	aws iam create-login-profile --user-name "$user" --password Mytec@2020 --password-reset-required --profile $AWS_PROFILE
+	aws iam add-user-to-group --user-name "$user" --group-name admin --profile $AWS_PROFILE
 done
